b58d49af81500f0f2afc4d37423c7660b3f13736
joseph <joseph@novanus.group>
2020-03-27 06:18:00 +0800

    Reset View Custom Control for Map.

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -9,6 +9,12 @@ export const COLOR_THEME_KEY = 'ncov-tracker-color';
 
 export const MAPS_TILE_LAYER_OPENSTREETMAP_URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 export const MAPS_TILE_LAYER_OPENSTREETMAP_ATTR = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+
+export const MAPS_TILE_LAYER_CARTOCDN_LIGHT_URL = 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
+export const MAPS_TILE_LAYER_CARTOCDN_LIGHT_ATTR= '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
+export const MAPS_TILE_LAYER_CARTOCDN_DARKURL = 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
+export const MAPS_TILE_LAYER_CARTOCDN_DARK_ATTR= '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
+
 export const MAPS_PH_CENTER_COOR = [11.449169, 123.439913];
 export const MAPS_PH_MIN_ZOOM_LEVEL = 5;
 export const MAPS_PH_FOCUS_ZOOM_LEVEL = 16;
